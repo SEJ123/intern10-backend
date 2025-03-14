fd341be9b566579419c46df90f3e0484595c4e76
eunjeong <jjj3080@naver.com>
2025-03-14 23:14:13 +0900

    초기 설정

---
+++ src/test/java/com/intern10/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern10.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
